b357f301679a3a26316a54d273a82ef1f9bf29be
Lu Daley <daleyluis@gmail.com>
2019-01-28 01:48:44 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -16,15 +16,3 @@ end
 
 #how do i select something that is true/based on boolean parameters?
 #if blank is true then puts blank.true
-
-=begin
-i = 0
-while i < array.size
-  if yield(array[i])
-     # shovel here if conditional fulfilled
-  end
-  #increment i
-end
-# etc.
-
-=end
